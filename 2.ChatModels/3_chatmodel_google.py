6f1fc4cec87db300148a82e309a13a669471b429
Yazdani, Mohammad Ajmal <ajmal.yazdani@gmail.com>
2025-03-04 20:10:51 +0530

    Revert "code"
    This reverts commit 1b1f5048a12052c91394748fb2d187f6b0cf0eef.

--- 2.ChatModels/3_chatmodel_google.py
+++ 2.ChatModels/3_chatmodel_google.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
 
 result = model.invoke("What is the capital of India")
 print(result.content)
